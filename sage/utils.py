df070f6c50349155265beeb076bf52163ecf90e3
Fahao Chen <chenfh612@gmail.com>
2022-03-01 21:50:15 +0900

    update

--- sage/utils.py
+++ sage/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from dgl import DGLGraph
 
-def Approx_prefix(input_features, parameter=10):
-    approx_results = input_features['features'][:, :parameter]
+def Approx_prefix(input_features, parameter=0.5):
+    scale = int(input_features['features'].size(1)*parameter)
+    approx_results = input_features['features'][:, :scale]
     return approx_results
