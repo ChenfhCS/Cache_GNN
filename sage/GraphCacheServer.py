df2e739b1e44c9c024be2c5a27aa18714c3015cc
Fahao Chen <chenfh612@gmail.com>
2022-03-01 16:08:17 +0900

    update

--- sage/GraphCacheServer.py
+++ sage/GraphCacheServer.py
@@ -14,8 +14,6 @@ class GraphCacheServer:
         self.device = device  # device
         self.node_num = node_num  # number of nodes in the graph
 
-        print(self.graph.ndata['features'])
-
         # self.graph.ndata['features'] = self.graph.ndata.pop('feat')
         # self.graph.ndata['labels'] = self.graph.ndata.pop('label')
 
