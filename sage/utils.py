a239ee34ad29374df9f17e88273f8d25855cbbe9
Fahao Chen <chenfh612@gmail.com>
2022-03-02 11:03:11 +0900

    update

--- sage/utils.py
+++ sage/utils.py
@@ -6,6 +6,6 @@ def Approx_prefix(input_features, parameter=0.5):
     '''
     Approx_results -> the first parameter*length values of the feature
     '''
-    scale = int(input_features['features'].size(1)*parameter)
-    approx_results = input_features['features'][:, :scale]
+    scale = int(input_features.size(1)*parameter)
+    approx_results = input_features[:, :scale]
     return approx_results
